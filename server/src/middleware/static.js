31ba8b43dba2dd95a2bf801eb1881fa039e1105e
longwang <longwang@paypal.com>
2022-05-25 19:07:19 +0800

    node env

--- server/src/middleware/static.js
+++ server/src/middleware/static.js
@@ -5,5 +5,11 @@ import path from "path";
  * 设置可访问目录
  */
 export default () => {
-  return koaStatic(path.resolve(__dirname, "../../../client/dist"));
+  let opts = {
+    maxage: 2160000000
+  }
+  if (process.env && process.env.NODE_ENV === 'development') {
+    opts = {}
+  }
+  return koaStatic(path.resolve(__dirname, "../../../client/dist"), opts);
 };
