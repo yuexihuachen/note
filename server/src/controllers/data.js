c2f550ee2c7d8b2c57318e635dc287cc85f4d5c8
果子树 <554638047@qq.com>
2022-07-29 20:09:21 +0800

    server delete encode

--- server/src/controllers/data.js
+++ server/src/controllers/data.js
@@ -54,7 +54,6 @@ async function setArticle(ctx, next) {
     return ctx.renderJson(result);
   }
   const { id, category, title, content, isPush } = ctx.request.body;
-  console.log(content)
   let response = null
   if (id) {
     response = await sqlite3.updateTableRows("article", {
@@ -68,7 +67,7 @@ async function setArticle(ctx, next) {
   } else {
     response = await sqlite3.insertTable("article", {
       title,
-      content: encodeURIComponent(content),
+      content,
       category_id: category,
       is_push: isPush,
     });
