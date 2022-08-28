3d8e3562b0d836bb96c93eeea4d8ec4adeba2118
果子树 <554638047@qq.com>
2022-08-28 13:43:57 +0800

    csp 内容安全策略

--- server/src/middleware/helmet.js
+++ server/src/middleware/helmet.js
@@ -4,5 +4,12 @@ const helmet = require('koa-helmet')
  * 通过设置 Http 头来使应用程序更加安全，避免常见的漏洞攻击 csrf xss
  */
 module.exports = () => {
-  return helmet()
+  return helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "img-src": ["'self'", "i.imgur.com"],
+      },
+    },
+  })
 }
